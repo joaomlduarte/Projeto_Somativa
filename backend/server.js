ce938d76a4c17c78054f58a83b9b6b2081e2bbba
João Duarte <joao84838@gmail.com>
2025-10-15 16:21:03 -0300

    Rodei as inicializacoes do backend

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+// server.js
+// Linha 1: carrega variaveis de ambiente (.env)
+require('dotenv').config()
+
+
+// importa libs principais
+const express = require('express')
+const cors = require('cors')
+
+
+// importa inicializacao do banco (criando tabelas/seed)
+const init
